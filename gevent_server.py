8681d023be039e203ae14af69c00a85df3e947eb
devin <yaohaohua1314@hotmail.com>
2015-05-18 16:22:18 +0800

    update

--- gevent_server.py
+++ gevent_server.py
@@ -4,7 +4,7 @@ __author__ = 'user'
 from gevent.wsgi import WSGIServer
 from baidurun import app
 
-# http_server = WSGIServer(('0.0.0.0', 8888), app)
-http_server = WSGIServer(('127.0.0.1', 5000), app)
+http_server = WSGIServer(('0.0.0.0', 8888), app)
+# http_server = WSGIServer(('127.0.0.1', 5000), app)
 
 http_server.serve_forever()
