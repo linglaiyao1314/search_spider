a0c5ce0ee9ec7b2727bd0f99fed86c83f7417c9a
devin <yaohaohua1314@hotmail.com>
2015-05-21 14:14:08 +0800

    修正post请求

--- baidurun.py
+++ baidurun.py
@@ -6,7 +6,7 @@ from engin.logs import search_logger
 
 
 app = Flask(__name__)
-app.debug = False
+app.debug = True
 
 HEADERS = {"User-Agent":
                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 Safari/537.36"}
@@ -23,22 +23,25 @@ def activity_api(itemlist):
 @app.route('/index/', methods=["POST", 'GET'])
 def json_result():
     search_logger.info(".........START to ..........Search...................\n")
-    if request.method == 'GET' or request.method == "POST":
+    keywords, country_id = ("", 1)
+    if request.method == 'POST':
+        keywords = request.form.get("keywords", "")
+        country_id = int(request.form.get("country_id", 1))
+    elif request.method == 'GET':
         keywords = request.args.get("keywords", None)
-        shop = request.args.get("shop", "all")
         country_id = int(request.args.get("country_id", 1))
-        search_logger.info("Keywords is : [ %s ]" % keywords)
-        if country_id == 1:
-            result = bdcrawl(keywords, headers=HEADERS, shop=shop)
-        elif country_id == 2:
-            result = pinglecrawl(keywords, headers=HEADERS, shop=shop)
-            if not result:
-                result = momocrawl(keywords, headers=HEADERS, shop=shop)
-            # result = momo_pc_event(keywords, headers=HEADERS, shop=shop)
-        else:
-            result = []
-        search_logger.debug("There %d items will be return\n\n" % len(result))
-        return json.dumps(activity_api(result))
+    search_logger.info("Keywords is : [ %s ]" % keywords)
+    if country_id == 1:
+        result = bdcrawl(keywords, headers=HEADERS)
+    elif country_id == 2:
+        result = pinglecrawl(keywords, headers=HEADERS)
+        if not result:
+            result = momocrawl(keywords, headers=HEADERS)
+        # result = momo_pc_event(keywords, headers=HEADERS, shop=shop)
+    else:
+        result = []
+    search_logger.debug("There %d items will be return\n\n" % len(result))
+    return json.dumps(activity_api(result))
 
 
 if __name__ == '__main__':
